d6827dc88ab8f976c445a6623ff46f5c2168c04f
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-11-14 14:58:28 +1000

    Start of testing

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance;
+
+    public bool p1;
+    public bool p2;
+    public Text myTurn;
+
+	// Use this for initialization
+	void Start () {
+		//p1
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
