53af5e044a3b96ed14f0d7b600a2c3cff35d8e20
MACLEODN\MacLeodn <1010746@student.sae.edu.au>
2017-12-13 03:16:07 +1000

    fiddled with my test scene(s) some more and commented my tile_movement scripts :)

--- Assets/MemberFolders/Narisa/Tile_Movement/scripts/TileType.cs
+++ Assets/MemberFolders/Narisa/Tile_Movement/scripts/TileType.cs
@@ -5,10 +5,10 @@ using System.Collections;
 public class TileType
 {
 
-    public string name;
-    public GameObject tileVisualPrefab;
+    public string name;  //name the tile in the inspector
+    public GameObject tileVisualPrefab;  //visual representation of the tile (e.g. green = grass, brown = dirt)
 
-    public bool isWalkable = true;
-    public float movementCost = 1;
+    public bool isWalkable = true; //is it walkable?
+    public float movementCost = 1;  //how much the tile cost, currently it costs one 
 
 }
