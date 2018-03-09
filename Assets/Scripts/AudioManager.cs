8ecd98761935ad4a83c33ec6971ebe38ff162d7a
SAE\sludinski2 <sonicbdp@gmail.com>
2018-03-09 12:41:58 +0800

    created audio manager script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	public static AudioManager Instance;
+
+
+	[FMODUnity.EventRef]
+	public string tankMove ="event:/Units/Tank/Tank Move";
+	FMOD.Studio.EventInstance TankMoveEv;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		TankMoveEv = FMODUnity.RuntimeManager.CreateInstance(tankMove);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	public void TankMove () 
+	{
+		//Write a reference to this script as "AudioManager.Instance.TankMove ();"
+		TankMoveEv.start ();
+		if (UnitScript.Instance.distanceRemain == 0)
+		{
+			TankMoveEv.stop ();
+		}
+	}
+}
