caa9239cdd680b5a995be157a2dc623f181e993f
Sam-PC\Sam <sonicbdp@gmail.com>
2018-04-18 16:25:04 +1000

    Big audio update - timing pass + new mech firing sound

---
+++ Assets/Scripts/AudioOneShots/Bunker/Lvl4/Bunker4AnimDesHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunker4AnimDesHandler : StateMachineBehaviour {
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		AudioManager.Instance.BunkerDes4();
+	}
+}
---
+++ Assets/Scripts/AudioOneShots/Flak/Lvl2/Flak2AnimDefHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flak2AnimDefHandler : StateMachineBehaviour {
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		AudioManager.Instance.FlakDefence2();
+	}
+}
