d00db6a3e7e50bae2638e0869382814927ede391
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2018-04-26 15:42:29 +1000

    Still working, push into more work

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+
+    public List<GameObject> NumberSprites;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+
+    public void SetCountdownValue(int newValue)
+    {
+
+
+        foreach (GameObject sprite in NumberSprites)
+        {
+            sprite.SetActive(false);            
+        }
+
+        NumberSprites[newValue] = NumberSprites[NumberSprites.Count];
+
+        NumberSprites[newValue].SetActive(true);
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
