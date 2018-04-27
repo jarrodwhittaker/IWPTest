df0abdd467a13ad0083f6c749ea56419b25be716
SAE\CClayton <1006749@student.sae.edu.au>
2018-04-27 15:18:50 +1000

    Creation of intro scene as well as edn credit scene

---
+++ Assets/Scripts/Introscene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Introscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void changeLevel()
+	{
+		SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+
+	}
+
+
+
+
+}
